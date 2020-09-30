9c5d770739e205eae58e0138d3016bb7a2ac2c87
John Melendez <john@Johns-MBP.domain>
2020-09-30 15:58:07 -0600

    Added text moment and hour , displayHour to script.js and started for loop.

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var displayHour = ["9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM"]
+
+for (var i = 0; i<hour.length; i++){
+    var createTextarea = $("<textarea>")
+    var creatediv = $("<div>")
+    
+}
